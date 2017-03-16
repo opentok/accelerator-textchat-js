290e5df875f14757860ea3a1b3ce41909a1fd6b1
Mike Thomas <mthomas@tokbox.com>
2017-03-16 15:35:03 +0100

    Update build script for new repo structure

--- build.sh
+++ build.sh
@@ -11,20 +11,12 @@ fi
 
 #Create zip file with JS and doc
 if [ "$task" == "-d" ]; then
-
-	if [[ -d opentok.js-text-chat ]]
-	then
-		cd opentok.js-text-chat
-		npm i
-		npm update
-		gulp dist
-		cd dist
-    gulp zip
-		exit 0
-	else
-		echo "Please run this script from 'JS'."
-		exit 1
-	fi
+	npm i
+	npm update
+	gulp dist
+	cd dist
+  gulp zip
+	exit 0
 fi
 
 echo Invalid parameters, please use ‘-t’ to run tests or ‘-d’ to create zip file with JS and doc.
